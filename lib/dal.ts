fbf907a2fe32d58a1f913a31bb7ef065330795b7
Alex <welmozhko88@gmail.com>
2025-05-26 13:02:59 +0300

    Aded auth data access functions

--- lib/dal.ts
+++ lib/dal.ts
@@ -1,3 +1,5 @@
+// Module stands for data access layer
+
 import { db } from '@/db'
 import { getSession } from './auth'
 import { eq } from 'drizzle-orm'
@@ -5,4 +7,47 @@ import { cache } from 'react'
 import { issues, users } from '@/db/schema'
 import { mockDelay } from './utils'
 
-export const getUserByEmail = async (email: string) => (email ? null : null)
+//____CURRENT_USER______________________________________
+export const getCurrentUser = async () => {
+  const session = await getSession()
+  if (!session) return null
+
+  try {
+    const result = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session.userId))
+
+    return result[0] || null
+  } catch (error) {
+    console.error('Error getting user by ID:', error)
+    return null
+  }
+}
+
+//____GET_USER_BY_EMAIL_________________________________
+export const getUserByEmail = async (email: string) => {
+  try {
+    const result = await db.select().from(users).where(eq(users.email, email))
+    return result[0] || null
+  } catch (error) {
+    console.error('Error getting user by email:', error)
+    return null
+  }
+}
+
+//____GET_ISSUES________________________________________
+export async function getIssues() {
+  try {
+    const result = await db.query.issues.findMany({
+      with: {
+        user: true,
+      },
+      orderBy: (issues, { desc }) => [desc(issues.createdAt)],
+    })
+    return result
+  } catch (error) {
+    console.error('Error fetching issues:', error)
+    throw new Error('Failed to fetch issues')
+  }
+}
